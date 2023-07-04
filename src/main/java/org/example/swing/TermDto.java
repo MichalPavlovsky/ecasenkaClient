ceb8842243bf6890f403dbd187df6e90ee263b60
Michal Pavlovsky <pavlovsky.miso@gmail.com>
2023-07-04 23:50:01 +0200

    termdto

---
+++ src/main/java/org/example/swing/TermDto.java
@@ -0,0 +1,33 @@
+package org.example.swing;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TermDto {
+
+    private String time;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    private String date;
+}
